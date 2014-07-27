7274e1df39c7245c60f87dde8badc372ab3472cd
Rémi <remi.alvado+git@gmail.com>
2014-07-27 11:46:56 +0200

    fix AbstractController interface

--- src/Acme/CalculatorBundle/Controller/AbstractController.php
+++ src/Acme/CalculatorBundle/Controller/AbstractController.php
@@ -3,6 +3,7 @@
 namespace Acme\CalculatorBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class AbstractController extends Controller
 {
